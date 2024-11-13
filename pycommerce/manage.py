5d9698ff5ca9798a6395a714ac8740cf21acddbe
Paolo Dapul <paolodapultech@gmail.com>
2024-11-13 13:31:21 +0000

    feat: create a project scaffold

--- pycommerce/manage.py
+++ pycommerce/manage.py
@@ -2,15 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 def main():
     """Run administrative tasks."""
